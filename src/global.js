058a50dff21a2cb2a5d6c163c0575db2c111b45a
Sandro Hagen <43218013+hugtrash@users.noreply.github.com>
2023-10-10 12:53:31 +0300

    implement mirror click script

--- src/global.js
+++ src/global.js
@@ -5,6 +5,12 @@ import bookingCart from './features/bookingCart'
 import buttonsDrawline from './features/buttonsDrawline'
 import './styles/buttonsDrawline.css'
 import customCursor from './features/customCursor'
+import insertScript from './utilities/insertScript'
+
+insertScript(
+  'https://cdn.jsdelivr.net/npm/@finsweet/attributes-mirrorclick@1/mirrorclick.js',
+  'body'
+)
 
 customCursor()
 backgroundColorFade()
