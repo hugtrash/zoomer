c26ae68884fb1a066fd8b4c8c4157b172f539a8f
Sandro Hagen <43218013+hugtrash@users.noreply.github.com>
2023-10-18 20:39:47 +0200

    another netlify minify test

--- src/home.js
+++ src/home.js
@@ -1,22 +1,22 @@
-import Splide from '@splidejs/splide'
+import Splide from '@splidejs/splide' // eslint-disable-line no-unused-vars
 
-import '@splidejs/splide/css/core'
-import './styles/splide.css'
-import brandMarquee from './features/brandMarquee'
-import './styles/brandMarquee.css'
+// import '@splidejs/splide/css/core'
+// import './styles/splide.css'
+// import brandMarquee from './features/brandMarquee'
+// import './styles/brandMarquee.css'
 
-brandMarquee()
+// brandMarquee()
 
-var splide = new Splide('.splide', {
-  type: 'slide',
-  autoWidth: true,
-  padding: { left: '2.5rem', right: '2.5rem' },
-  pagination: false,
-  speed: 600,
-  easing: 'ease',
-  autoplay: true,
-  interval: 4000,
-  paginationKeyboard: true,
-  snap: false,
-})
-splide.mount()
+// var splide = new Splide('.splide', {
+//   type: 'slide',
+//   autoWidth: true,
+//   padding: { left: '2.5rem', right: '2.5rem' },
+//   pagination: false,
+//   speed: 600,
+//   easing: 'ease',
+//   autoplay: true,
+//   interval: 4000,
+//   paginationKeyboard: true,
+//   snap: false,
+// })
+// splide.mount()
