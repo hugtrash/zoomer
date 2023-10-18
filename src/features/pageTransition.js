9aa085a7c61b61895fd6aefa70ac5c68df9da3b1
Sandro Hagen <43218013+hugtrash@users.noreply.github.com>
2023-10-18 16:03:00 +0200

    maybe finally found a way the linter to ignore missing $ ...

--- src/features/pageTransition.js
+++ src/features/pageTransition.js
@@ -1,53 +1,49 @@
-import $ from 'jquery'
-
 function pageTransition() {
-  $(document).ready(function ($) {
-    let transitionTrigger = $('.transition_trigger')
-    let introDurationMS = 1000
-    let exitDurationMS = 1150
-    let excludedClass = 'no-transition'
+  let transitionTrigger = $('.transition_trigger')
+  let introDurationMS = 1000
+  let exitDurationMS = 1150
+  let excludedClass = 'no-transition'
 
-    // On Page Load
-    if (transitionTrigger.length > 0) {
-      transitionTrigger.click()
-      $('body').addClass('no-scroll-transition')
-      setTimeout(() => {
-        $('body').removeClass('no-scroll-transition')
-      }, introDurationMS)
-    }
-    // On Link Click
-    $('a').on('click', function (e) {
-      if (
-        $(this).prop('hostname') == window.location.host &&
-        $(this).attr('href').indexOf('#') === -1 &&
-        !$(this).hasClass(excludedClass) &&
-        $(this).attr('target') !== '_blank' &&
-        transitionTrigger.length > 0
-      ) {
-        e.preventDefault()
-        $('body').addClass('no-scroll-transition')
-        let transitionURL = $(this).attr('href')
-        transitionTrigger.click()
-        setTimeout(function () {
-          window.location = transitionURL
-        }, exitDurationMS)
-      }
-    })
-    // On Back Button Tap
-    window.onpageshow = function (event) {
-      if (event.persisted) {
-        window.location.reload()
-      }
-    }
-    // Hide Transition on Window Width Resize
+  // On Page Load
+  if (transitionTrigger.length > 0) {
+    transitionTrigger.click()
+    $('body').addClass('no-scroll-transition')
     setTimeout(() => {
-      $(window).on('resize', function () {
-        setTimeout(() => {
-          $('.transition').css('display', 'none')
-        }, 50)
-      })
+      $('body').removeClass('no-scroll-transition')
     }, introDurationMS)
+  }
+  // On Link Click
+  $('a').on('click', function (e) {
+    if (
+      $(this).prop('hostname') == window.location.host &&
+      $(this).attr('href').indexOf('#') === -1 &&
+      !$(this).hasClass(excludedClass) &&
+      $(this).attr('target') !== '_blank' &&
+      transitionTrigger.length > 0
+    ) {
+      e.preventDefault()
+      $('body').addClass('no-scroll-transition')
+      let transitionURL = $(this).attr('href')
+      transitionTrigger.click()
+      setTimeout(function () {
+        window.location = transitionURL
+      }, exitDurationMS)
+    }
   })
+  // On Back Button Tap
+  window.onpageshow = function (event) {
+    if (event.persisted) {
+      window.location.reload()
+    }
+  }
+  // Hide Transition on Window Width Resize
+  setTimeout(() => {
+    $(window).on('resize', function () {
+      setTimeout(() => {
+        $('.transition').css('display', 'none')
+      }, 50)
+    })
+  }, introDurationMS)
 }
 
 export default pageTransition
