e35f312b39c51294f3f0f3b12e31e63399f8837f
Sandro Hagen <43218013+hugtrash@users.noreply.github.com>
2023-10-27 00:35:59 +0800

    add finsweet cms static

--- src/faces.js
+++ src/faces.js
@@ -1,4 +1,5 @@
 import facesFilterActive from './features/facesFilterActive'
 facesFilterActive()
 import insertScript from './utilities/insertScript'
-insertScript('https://cdn.jsdelivr.net/npm/@finsweet/attributes-cmsfilter@1/cmsfilter.js', 'body')
+insertScript('https://cdn.jsdelivr.net/npm/@finsweet/attributes-cmsfilter@1/cmsfilter.js', 'body', 'async')
+insertScript('https://cdn.jsdelivr.net/npm/@finsweet/attributes-cmsstatic@1/cmsstatic.js', 'body', 'async')
