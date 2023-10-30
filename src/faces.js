c9d9bd7c9446ac8ab24eaa57e9f6215748dbe9e0
Sandro Hagen <43218013+hugtrash@users.noreply.github.com>
2023-10-30 17:23:36 +0800

    change insert script insertion and

--- src/faces.js
+++ src/faces.js
@@ -1,5 +1,5 @@
 import facesFilterActive from './features/facesFilterActive'
 facesFilterActive()
-import insertScript from './utilities/insertScript'
+import insertScript from './features/insertScript'
 insertScript('https://cdn.jsdelivr.net/npm/@finsweet/attributes-cmsfilter@1/cmsfilter.js', 'body', 'async')
 insertScript('https://cdn.jsdelivr.net/npm/@finsweet/attributes-cmsstatic@1/cmsstatic.js', 'body', 'async')
